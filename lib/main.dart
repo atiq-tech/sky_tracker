9c40fd53f6c7ab689b5c63a97613f83247e3ee0f
Your Name <you@example.com>
2023-04-13 16:22:16 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sky_tracker/providers/counter_probider.dart';
+import 'package:sky_tracker/views/deshboard_screen.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CounterProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: const DeshBoardScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/views/auth/reset_password_screen.dart
@@ -0,0 +1,52 @@
+// import 'package:flutter/material.dart';
+// import 'package:sky_tracker/business_logics/auth.dart';
+// import 'package:sky_tracker/widget/button.dart';
+
+
+// class ResetPassword extends StatelessWidget {
+//   ResetPassword({super.key});
+//   final TextEditingController _emailController = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Padding(
+//           padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               const Text(
+//                 'Forgot your password?',
+//                 style: TextStyle(fontSize: 38, fontWeight: FontWeight.w600),
+//               ),
+//               const Text(
+//                 'enter your email and reset password.',
+//                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+//               ),
+//               const SizedBox(height: 50),
+//               TextFormField(
+//                 controller: _emailController,
+//                 decoration: const InputDecoration(
+//                   hintText: 'email address',
+//                   prefixIcon: Icon(
+//                     Icons.email_outlined,
+//                   ),
+//                 ),
+//               ),
+//               const Divider(color: Colors.transparent),
+//               PurpleButton("Rest Now", () {
+//                 if (_emailController.text.isNotEmpty) {
+//                   Auth.resetPassword(
+//                     _emailController.text,
+//                     context,
+//                   );
+//                 }
+//                 _emailController.clear();
+//               }),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
